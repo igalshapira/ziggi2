ece75cf26336f50c55cd38dbe9cdc8076130f6e6
Igal Shapira <igal.shapira@gmail.com>
2018-10-09 10:37:56 +0300

    Issue #4 - Fix share link

--- app/helpers/share_helper.rb
+++ app/helpers/share_helper.rb
@@ -2,7 +2,7 @@ module ShareHelper
   def user_share_link(user, hash)
     # UHash is user.id + sha1("YA" + user.email + "YA")
     uhash = user.id.to_s + Digest::SHA1.hexdigest("YA" + user.email + "YA")
-    return 'http://www.ziggi.co/share/' + uhash + '/' + hash
+    return 'http://www.ziggi.co.il/share/' + uhash + '/' + hash
   end
 
   #get 'share/:uhash/:hash' => 'share#share'
